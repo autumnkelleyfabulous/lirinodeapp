b9085773a53394e0f1fbc696b9940843e8ca26ab
Autumn Kelley Barr <autumnkelleyfabulous@Autumns-iMac.local>
2019-02-06 17:43:08 -0700

    Add movie-this function

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
+// liriBot
+// LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+
 require("dotenv").config();
 
 
@@ -27,77 +31,135 @@ for (var i = 2; i < nodeAargs.length; i++) {
     }
 }
 console.log("I Will Posses Your Heart")
-    // start
-    function liriBot(concert, spotify, movie) {
-        // var command = process.argv[2];
+// start
+function liriBot(concert, spotify, movie) {
+    // var command = process.argv[2];
 
-        switch (liriReturn) {
+    switch (liriReturn) {
 
-            case "spotify-this-song":
-                spotifyThisSong();
-                break;
+        case "spotify-this-song":
+            spotifyThisSong();
+            break;
 
 
-            case "movie-this":
-                moviethis();
-                break;
+        case "movie-this":
+            moviethis();
+            break;
 
-            default: console.log("spotify-this-song");
-                console.log("movie-this");
+        default: console.log("spotify-this-song");
+            console.log("movie-this");
 
-        }
     }
-        // SpotifyThisSong
-        // preview song from spotify-using spotify Api calls
-        var spotify = new liriBot()
-        function spotifyThisSong(trackName) {
-            process.env.spotify
+}
+// SpotifyThisSong
+// preview song from spotify-using spotify Api calls
+var spotify = new liriBot()
+function spotifyThisSong(trackName) {
+    process.env.spotify
 
-            var trackName = process.argv(3);
-            var nodeArgs = process.argv;
-            var trackName = "";
-            if (trackName) {
-                trackName = "I Will Posses Your Heart";
+    var trackName = process.argv[3];
+    var nodeArgs = process.argv;
+    var trackName = "";
+    if (trackName) {
+        trackName = "I Will Posses Your Heart";
 
-            };
+    };
 
-            songRequest = trackName;
-            spotify.search({
-                type: "track",
-                query: songRequest
-            })
-                .then(function (response) {
-                    console.log(response);
+    songRequest = trackName;
+    spotify.search({
+        type: "track",
+        query: songRequest
+    })
+        .then(function (response) {
+            console.log(response);
 
-                    if (err) throw err;
-                    // .catch(function)(err) {
-                    //     console.log(err);
-                    // function (err, data) {
-                    //     if (err) {
-                    var trackInfo = data.tracks.items;
-                    for (var i = 0; i = 5; i--) {
-                        if (trackInfo[i] = undefined) {
-                            var spotifyResults
-                            Artist: " + trackInfo[i].artists[0].name"
-                            Song: " = trackInfo[i].name"
-                            previewUrl: " +trackInfo[i].preview_url"
-                            Album: " = trackInfo[i].album.name"
-                        }
-                    }
-                    
-
-                    if (trackName === "") {
-                        spotify.search({
-                            type: 'track',
-                            query: "the sign"
-                        })
-                        console.log(spotifyResults);
-                        console.log('');
-                    } 
-                            else {
-                            console.log("can't find song");   
-                            }   
-                    })
+            if (err) throw err;
+            // .catch(function)(err) {
+            //     console.log(err);
+            // function (err, data) {
+            //     if (err) {
+            var trackInfo = data.tracks.items;
+            for (var i = 0; i = 5; i--) {
+                if (trackInfo[i] = undefined) {
+                    var spotifyResults
+                    Artist: " + trackInfo[i].artists[0].name"
+                    Song: " = trackInfo[i].name"
+                    previewUrl: " +trackInfo[i].preview_url"
+                    Album: " = trackInfo[i].album.name"
                 }
-            
-                liriBot(spotify);
+            }
+
+
+            if (trackName === "") {
+                spotify.search({
+                    type: 'track',
+                    query: "the sign"
+                })
+                console.log(spotifyResults);
+                console.log('');
+            }
+            else {
+                console.log("can't find song");
+            }
+        })
+}
+
+liriBot(spotify);
+
+
+// OMDB MOVIE API-MOVIE THIS
+var moviethis = new liriBot()
+function movieThis() {
+    var nodeArgs = process.argv;
+    var queryUrl = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function (error, response, body) {
+        if (error && response, statusCode === 200) {
+            console.log("anything")
+            var myMovieData = JSON.parse(body);
+            var queryUrl = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+        }
+
+    })
+
+    var movieTitle = "";
+
+    // request(queryUrl, function(error, response,body):
+    // if (error && response, statusCode === 200
+
+    var moviedata = Json.parse
+    // title = (Title,Year,Rating,language, plot, actors);
+
+    axios.get(urlHit).then(
+        function (response) {
+            var jsonData = response.data;
+
+            console.log("Title: " + jsonData.Title);
+            console.log("Year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country: " + jsonData.Country);
+            console.log("Language: " + jsonData.Language);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+            console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+        }
+    );
+};
+
+
+
+if (movieName === "") {
+    movie.search({
+        type: "its on Netflix!",
+        query: "Mr.Nobody"
+    })
+    console.log(moviethis);
+    console.log('');
+}
+else {
+    console.log("Mr.Nobody");
+}
+
+
+
+liriBot(moviethis);
